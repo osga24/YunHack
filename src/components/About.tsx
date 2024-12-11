6cf6842389fdfcc76e1eec924483c3029008ea25
Os24 <oscarhuang950324@gmail.com>
2024-12-11 13:21:19 +0800

    renew title and ico

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,10 +13,10 @@ export default function About() {
           <div className="flex flex-col justify-center items-center text-center px-5 md:py-0">
             <h1 className='md:text-5xl sm:text-3xl text-2xl font-bold py-2'>關於我們！</h1>
             <div className='text-xl font-bold'>
-              <p>雲科資安社是由 <span className='text-green-400'>黃宥睿、郭泓毅、蘇育愷</span></p>
-              <p>在得知入學 <span className='text-[#469491]'>雲林科技大學</span> 就計劃一起創辦的學術性社團</p>
-              <p>在這邊不用怕你完全沒接觸過，我們會從零基礎開始帶起</p>
-              <p>讓你一步步與我們進入資安的世界！</p>
+				<p>雲科資安社是由 <span className='text-green-400'>黃宥睿、郭泓毅、蘇育愷</span></p>
+				<p>在得知入學 <span className='text-[#469491]'>雲林科技大學</span> 就計劃一起創辦的學術性社團</p>
+				<p>在這邊不用怕你完全沒接觸過，我們會從零基礎開始帶起</p>
+				<p>讓你一步步與我們進入資安的世界！</p>
             </div>
           </div>
         </div>
