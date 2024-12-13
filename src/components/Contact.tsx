90e056e0bdf92e57163294a7b2e272a4f08a41bd
Os24 <oscarhuang950324@gmail.com>
2024-12-13 16:56:50 +0800

    reset mail

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -16,7 +16,7 @@ export default function Contact() {
         {/* IG Section */}
         <div className='flex flex-row items-center space-x-4 transform transition-transform duration-300 hover:scale-110'>
           {/* IG Photo */}
-          <div className='w-[60px] h-[60px] md:w-[80px] md:h-[80px]'>
+          <div className='w-[50px] h-[50px] md:w-[80px] h-[80px]'>
             <a href='https://www.instagram.com/nyust_hacking/'>
               <img
                 className='w-full h-full rounded-lg object-cover'
@@ -36,7 +36,7 @@ export default function Contact() {
         {/* Mail Section */}
         <div className='flex flex-row items-center space-x-4 transform transition-transform duration-300 hover:scale-110'>
           {/* Mail Icon */}
-          <div className='w-[60px] h-[60px] md:w-[80px] md:h-[80px]'>
+          <div className='w-[80px] h-[80px]'>
             <img
               className='w-full h-full rounded-lg object-cover'
               src='/images/mail_icon.png'
@@ -47,14 +47,14 @@ export default function Contact() {
           {/* Email Address */}
           <div className='text-xl font-semibold md:text-lg'>
             <p>Contact us via Email:</p>
-            <a href='mailto:yunhack@啥.tw' className='text-blue-500'>nyust.hacking@gmail.com</a>
+            <a href='mailto:nyust.hacking@gmail.com' className='text-blue-500'>yunhack@啥.tw</a>
           </div>
         </div>
 
         {/* Discord Section */}
         <div className='flex flex-row items-center space-x-4 transform transition-transform duration-300 hover:scale-110'>
           {/* Discord Icon */}
-          <div className='w-[60px] h-[60px] md:w-[80px] md:h-[80px]'>
+          <div className='w-[80px] h-[80px]'>
             <a href='https://discord.gg/vxgqgpSmBj'>
               <img
                 className='w-full h-full rounded-lg object-cover'
